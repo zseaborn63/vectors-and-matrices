8c70f22851e504cb48c214a267d5465df486e1b7
taddeimania <jtaddei@gmail.com>
2015-06-01 14:03:23 -0400

    Added function stubs

--- matrix_math.py
+++ matrix_math.py
@@ -1,2 +1,56 @@
 class ShapeException(Exception):
     pass
+
+
+def dot():
+    pass
+
+
+def magnitude():
+    pass
+
+
+def shape():
+    pass
+
+
+def vector_add():
+    pass
+
+
+def vector_sub():
+    pass
+
+
+def vector_sum():
+    pass
+
+
+def vector_multiply():
+    pass
+
+
+def vector_mean():
+    pass
+
+
+def matrix_row():
+    pass
+
+
+def matrix_col():
+    pass
+
+
+def matrix_scalar_multiply():
+    pass
+
+
+def matrix_vector_multiply():
+    pass
+
+
+def matrix_matrix_multiply():
+    pass
+
+
